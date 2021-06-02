ab16c07b4ede3fb086bf36d80fc8ad9ce664476c
nunzio <chiefa.nunzio@gmail.com>
2021-06-02 20:40:09 +0200

    fix error

---
+++ app/Task.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [   
+
+        'name'   
+        
+    ];
+
+    public function employees() {
+
+        return $this -> belongsToMany(Employee::class);   
+    }
+}
---
+++ database/seeds/TaskSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Employee;
+use App\Task;   
+class TaskSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Task::class, 200) -> create()
+            -> each(function($task){
+
+                $employees = Employee::inRandomOrder()
+                    ->limit(rand(2, 10))
+                    ->get();
+                $task -> employees() -> attach($employees);   
+   
+                $task -> save();
+            });   
+    }   
+}
